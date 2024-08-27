7942eb32044c6c0a41b7259ce1f80755950a57cf
mthodoris <thodorisman@gmail.com>
2024-08-27 13:17:37 +0300

    fix bugs for install

--- venv_setup.sh
+++ venv_setup.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+
 # Set the name of the virtual environment
 venv_name="venv"
 
@@ -14,6 +15,8 @@ echo "Virtual environment '$venv_name' activated"
 # Install the requirements
 echo "Install the requirements"
 pip3 install -r requirements.txt
+# Install Pytorch
+pip install torch==1.9.0+cpu torchvision==0.10.0+cpu torchaudio==0.9.0 -f https://download.pytorch.org/whl/torch_stable.html
 
 # Notify the user
 echo "Virtual environment '$venv_name' created and activated."
